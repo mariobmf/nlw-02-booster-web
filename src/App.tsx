98a6308588baf88cc7123032e0fbfd224fa7847d
Mário Fernandes <mariobmf02@gmail.com>
2020-08-06 16:00:01 -0300

    ESLint and Prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Inital Commit</h1>
     </div>
   );
-}
+};
 
 export default App;
