8691b2a0034b685521d8b0925a89fdcd0cd7eb88
Mário Fernandes <mariobmf02@gmail.com>
2020-08-16 11:01:16 -0300

    Integration of the TeacherList page with the api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,42 +1,48 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import wppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-const TeacherItem: React.FC = () => {
+export interface ITeacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  name: string;
+  subject: string;
+  whatsapp: string;
+  cost: number;
+}
+interface IProps {
+  teacher: ITeacher;
+}
+
+const TeacherItem: React.FC<IProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars0.githubusercontent.com/u/40251156?s=400&u=b0d454395ee494219b4464fa36a922fba5a70ac7&v=4"
-          alt="Mário Fernandes"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Mário Fernandes</strong>
-          <span>Engenharia de Software</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat
-        augue magna, a suscipit leo consectetur nec. Donec mi dui, ultricies a
-        est id, accumsan interdum neque. Maecenas et lacus dolor.
-        <br />
-        <br />
-        Pellentesque malesuada molestie nisi, nec mollis ipsum sagittis in. Cras
-        commodo ipsum vel egestas consequat. Fusce sit amet pellentesque tellus.
-        Nunc volutpat, velit a tincidunt lacinia, diam libero elementum neque,
-        vitae aliquam libero erat sed nunc.
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          type="button"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={wppIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { ITeacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 
 import './styles.css';
+import api from '../../services/api';
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState<ITeacher[]>([]);
+  const [subject, setSubject] = useState('');
+  const [weekDay, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  const searchTeachers = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const response = await api.get<ITeacher[]>('classes', {
+      params: {
+        subject,
+        week_day: weekDay,
+        time,
+      },
+    });
+
+    setTeachers(response.data);
+  };
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
           <Select
             name="subject"
             label="Matéria"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
             options={[
               { value: 'Artes', label: 'Artes' },
               { value: 'Biologia', label: 'Biologia' },
@@ -30,6 +52,8 @@ const TeacherList: React.FC = () => {
           <Select
             name="week_day"
             label="Dia da semana"
+            value={weekDay}
+            onChange={(e) => setWeekDay(e.target.value)}
             options={[
               { value: '0', label: 'Domingo' },
               { value: '1', label: 'Segunda-feira' },
@@ -40,12 +64,21 @@ const TeacherList: React.FC = () => {
               { value: '6', label: 'Sábado' },
             ]}
           />
-          <Input name="time" label="Hora" type="time" />
+          <Input
+            name="time"
+            label="Hora"
+            type="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
       <main>
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} />
+        ))}
       </main>
     </div>
   );
